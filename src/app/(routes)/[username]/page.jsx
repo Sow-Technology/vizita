ffd004c0b8a078e9195e078aac892c8d7183ba99
KanishKumar11 <kanishkumar11@outlook.in>
2024-02-03 19:53:23 +0530

    backend structure ready

--- src/app/(routes)/[username]/page.jsx
+++ src/app/(routes)/[username]/page.jsx
@@ -1,13 +1,15 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import axios from "axios";
 const Page = () => {
   const pathname = usePathname();
   const username = pathname.substring(1);
+  const [data, setData] = useState({});
   const searchUser = async () => {
     try {
       const response = await axios.post("/api/user", { username });
+      setData(response.data.data);
       console.log(response.data);
     } catch (error) {
       console.error("Error:", error);
@@ -20,6 +22,7 @@ const Page = () => {
     <div>
       <h1>User Profile Page</h1>
       <p>Username: {username}</p>
+      {data.personalEmail}
     </div>
   );
 };
